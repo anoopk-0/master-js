3fba7ed8a09974c26d075a604c36de28ca2c0cc7
anoopk-0 <anp.k072@gmail.com>
2023-02-13 16:17:33 +0530

    refactor

--- 08_Advance/currying.js
+++ 08_Advance/currying.js
@@ -1,5 +1,8 @@
 /*
 Currying is a process in functional programming in which we can transform a function with multiple arguments into a sequence of nesting functions. It returns a new function that expects the next argument inline.
+
+  - Currying helps we avoid passing the same variable again and again.
+  - It helps to create a higher order function
 */
 
 //example:create a function with multiple of 2 or 3 or n...
